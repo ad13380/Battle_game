2f82441d38ab5732bb03736cf7e697a64d5bec64
Anthony <a.a.donovan@outlook.com>
2020-05-28 16:38:47 +0100

    Adds capability to enter players

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   # ... app code here ...
   get "/" do
-    "Testing infrastructure working!"
+    erb(:index)
+  end
+
+  post "/names" do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,11 +1,18 @@
 require './app'
 
-
-
-
-feature "testing" do
+feature "outputs Testing infrastructure working! string" do
   it "returns Testing infrastructure working!" do
     visit '/'
     expect(page).to have_content("Testing infrastructure working!")
+  end
+end
+
+feature "except input and return onto screen" do
+  it "displays player names after they fill in form and submit" do
+    visit '/'
+    fill_in 'player_1', with: 'Steve'
+    fill_in 'player_2', with: 'John'
+    click_button 'Submit'
+    expect(page).to have_content("Player 1: Steve vs Player 2: John")
   end
 end
